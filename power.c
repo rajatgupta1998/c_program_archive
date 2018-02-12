935512ea60f32a921554bd024661e64e3f37c541
Rajat Gupta <rjtgupta84@gmail.com>
2018-02-12 21:25:25 +0530

    Add pow function .c file
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

---
+++ power.c
@@ -0,0 +1,22 @@
+Take a base value and exponent as an input from user. Write a program to find the power of a given number.
+
+Sample Input:
+
+10
+
+2
+
+Sample Output:
+
+100
+
+#include <stdio.h>
+
+int main(void) {
+    int var1,var2,var3;
+    scanf("%d",&var1); scanf("%d",&var2);
+    var3=pow(var1,var2);
+    printf("%d",var3);
+    
+    return 0;
+}

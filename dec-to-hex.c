96b8fc001d619f733943a5ea96fe8a8c46f4b30b
Rajat Gupta <rjtgupta84@gmail.com>
2018-02-13 14:40:17 +0530

    Add decimal to hexadecimal converter
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

---
+++ dec-to-hex.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    int number;
+    printf("Enter number in Decimal\n");
+    scanf("%d",&number);
+    printf("Number in Hexa is %X\n",number);
+    return 0;
+}

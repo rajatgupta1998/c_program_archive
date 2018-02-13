a254c8c9705f1ac463aa4d46f5e32d8f9f752d9d
Rajat Gupta <rjtgupta84@gmail.com>
2018-02-13 13:42:51 +0530

    Add program for palindrome check. * Kang from pichord's repo
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+    int number, reverse = 0, remainder, store;
+    printf("Please enter a number: \n");
+    scanf("%d", &number);
+    store = number; 
+    while (number!=0) 
+    {
+        remainder = number % 10;
+        reverse = (reverse*10) + remainder;
+        number /= 10;
+    }
+
+    if (reverse == store)   //If the value of thhe reversed number is equal to the original number we stored, print the following:
+        printf("This is a palindrome");
+    else    //If not, print the following:
+        printf("This is not a palindrome\n");
+    }
